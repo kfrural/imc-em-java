c46808e7065ddc15614ab371aea7d867b0d20e54
Karla C. Ferreira <karlinharural@gmail.com>
2023-08-30 15:15:30 -0300

    Update projeto.java

--- projeto.java
+++ projeto.java
@@ -1,77 +1,159 @@
-String message = ("Aqui será calculado o imc");
-	JOptionPane.showMessageDialog(null, message);
+import java.util.Scanner;
 
-	Scanner input = new Scanner(System.in);
+ 
 
-	System.out.print("Defina seu sexo \n Para masculino digite [1]. \n Para feminino digite [2]. \n");		
-	sexo = input.nextInt(); // salva na váriavel sexo.
-	nome_sexo = sexo;
-		
-	if (nome_sexo == 1)
-	{
-		System.out.print("-  masculino!");
-	}
+public class Main {
 
-	if (nome_sexo == 2)
-	{
-		System.out.print("- feminino!");
-	}
+    public static void main(String[] args) {
 
-	System.out.print("\n\n Qual é o seu peso [Exemplo: 60,5]?\n");		
-	peso = input.nextDouble(); // salva na váriavel peso.
-	System.out.printf("- O seu peso é: %6.1f kilogramas!", peso);
+        String message = "Aqui será calculado o imc";
 
-	System.out.print("\n\n Qual é a sua altura [Exemplo: 1,70]?\n");		
-	altura = input.nextDouble(); // salva na váriavel altura.
-	System.out.printf("- A sua altura é: %6.2f metros!", altura);
+        System.out.println(message);
 
-	calcular_IMC = peso / ((altura)*(altura)); // calcula o valor do IMC
+ 
 
-	if (sexo == 1)
-	{
-		if (calcular_IMC < h_imc[0])
-		{
-			System.out.print("\n\n Resultado IMC (Masculino):");
-			System.out.printf("\n- O seu IMC é: %6.1f. Você está abaixo do peso!", calcular_IMC);
-			System.out.print(h_resultado);
-		}
+        Scanner input = new Scanner(System.in);
 
-		if ((calcular_IMC > h_imc[0]) && (calcular_IMC < h_imc[1]))
-		{
-			System.out.print("\n\n Resultado IMC (Masculino):");
-			System.out.printf("\n- O seu IMC é: %6.1f. Você está no peso ideal!", calcular_IMC);
-			System.out.print(h_resultado);
-		}
+        int sexo, nome_sexo;
 
-		if (calcular_IMC > h_imc[1])
-		{
-			System.out.print("\n\n Resultado IMC (Masculino):");
-			System.out.printf("\n- O seu IMC é: %6.1f. Você está acima do peso!", calcular_IMC);
-			System.out.print(h_resultado);
-		}
-	}
+        double peso, altura, calcular_IMC;
 
-	if (sexo == 2)
-	{
-		if (calcular_IMC < m_imc[0])
-		{
-			System.out.print("\n\n Resultado IMC (Feminino):");
-			System.out.printf("\n- O seu IMC é: %6.1f. Você está abaixo do peso!", calcular_IMC);
-			System.out.print(m_resultado);
-		}
+        double[] h_imc = {20.7, 26.4};
 
-		if ((calcular_IMC > m_imc[0]) && (calcular_IMC < m_imc[1]))	
-		{	
-			System.out.print("\n\n Resultado IMC (Feminino):");	
-			System.out.printf("\n- O seu IMC é: %6.1f. Você está no peso ideal!", calcular_IMC);
-			System.out.print(m_resultado);
-		}
+        double[] m_imc = {19.1, 25.8};
 
-		if (calcular_IMC > m_imc[1])
-		{
-			System.out.print("\n\n Resultado IMC (Feminino):");
-			System.out.printf("\n- O seu IMC é: %6.1f. Você está acima do peso!", calcular_IMC);
-			System.out.print(m_resultado);
-		}
-	}	
+        String h_resultado = "- Resultado para homens";
+
+        String m_resultado = "- Resultado para mulheres";
+
+ 
+
+        System.out.println("Defina seu sexo");
+
+        System.out.println("Para masculino digite [1].");
+
+        System.out.println("Para feminino digite [2].");
+
+        sexo = input.nextInt(); // salva na variável sexo.
+
+        nome_sexo = sexo;
+
+ 
+
+        if (nome_sexo == 1) {
+
+            System.out.println("- masculino!");
+
+        }
+
+ 
+
+        if (nome_sexo == 2) {
+
+            System.out.println("- feminino!");
+
+        }
+
+ 
+
+        System.out.println("\n\nQual é o seu peso [Exemplo: 60.5]?");
+
+        peso = input.nextDouble(); // salva na variável peso.
+
+        System.out.printf("- O seu peso é: %6.1f kilogramas!", peso);
+
+ 
+
+        System.out.println("\n\nQual é a sua altura [Exemplo: 1.70]?");
+
+        altura = input.nextDouble(); // salva na variável altura.
+
+        System.out.printf("- A sua altura é: %6.2f metros!", altura);
+
+ 
+
+        calcular_IMC = peso / (altura * altura); // calcula o valor do IMC
+
+ 
+
+        if (sexo == 1) {
+
+            if (calcular_IMC < h_imc[0]) {
+
+                System.out.println("\n\nResultado IMC (Masculino):");
+
+                System.out.printf("- O seu IMC é: %6.1f. \n Você está abaixo do peso!", calcular_IMC);
+
+                System.out.println(h_resultado);
+
+            }
+
+ 
+
+            if ((calcular_IMC > h_imc[0]) && (calcular_IMC < h_imc[1])) {
+
+                System.out.println("\n\nResultado IMC (Masculino):");
+
+                System.out.printf("- O seu IMC é: %6.1f.\n Você está no peso ideal!", calcular_IMC);
+
+                System.out.println(h_resultado);
+
+            }
+
+ 
+
+            if (calcular_IMC > h_imc[1]) {
+
+                System.out.println("\n\nResultado IMC (Masculino):");
+
+                System.out.printf("- O seu IMC é: %6.1f.\n Você está acima do peso!", calcular_IMC);
+
+                System.out.println(h_resultado);
+
+            }
+
+        }
+
+ 
+
+        if (sexo == 2) {
+
+            if (calcular_IMC < m_imc[0]) {
+
+                System.out.println("\n\nResultado IMC (Feminino):");
+
+                System.out.printf("- O seu IMC é: %6.1f.\n Você está abaixo do peso!", calcular_IMC);
+
+                System.out.println(m_resultado);
+
+            }
+
+ 
+
+            if ((calcular_IMC > m_imc[0]) && (calcular_IMC < m_imc[1])) {
+
+                System.out.println("\n\nResultado IMC (Feminino):");
+
+                System.out.printf("- O seu IMC é: %6.1f.\n Você está no peso ideal!", calcular_IMC);
+
+                System.out.println(m_resultado);
+
+            }
+
+ 
+
+            if (calcular_IMC > m_imc[1]) {
+
+                System.out.println("\n\nResultado IMC (Feminino):");
+
+                System.out.printf("- O seu IMC é: %6.1f.\n Você está acima do peso!", calcular_IMC);
+
+                System.out.println(m_resultado);
+
+            }
+
+        }
+
     }
+
+}
